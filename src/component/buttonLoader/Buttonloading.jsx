8b3ff6b60ecab4411553b8984028f017a408b188
toye1990 <ifetoyeobafemi@gmail.com>
2025-11-03 23:27:32 +0100

    loader updated

---
+++ src/component/buttonLoader/Buttonloading.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import "./Buttonloading.css";
+
+const Buttonloading = () => {
+  return (
+    <div className="dot-spinner">
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+    </div>
+  );
+};
+
+export default Buttonloading;

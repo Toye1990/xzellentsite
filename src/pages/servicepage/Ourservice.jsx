a3ae2101cfaa0ab122844dd53bd887e9c9af0df8
toye1990 <ifetoyeobafemi@gmail.com>
2026-05-02 09:14:03 +0100

    upd

--- src/pages/servicepage/Ourservice.jsx
+++ src/pages/servicepage/Ourservice.jsx
@@ -96,12 +96,14 @@ const Ourservice = () => {
                 <span className="badge bg-light text-dark mb-3">
                   Available Now
                 </span>
-                <Link
-                  to="/placeorder"
-                  className="btn btn-primary w-100 mt-auto"
+                <a
+                  href="https://www.xzellentstore.com/shop"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-success w-100 mt-auto"
                 >
-                  Place Order
-                </Link>
+                  Shop Now
+                </a>
               </div>
             </div>
           </div>
